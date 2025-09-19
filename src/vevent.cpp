29c24083c921c0105f76235404940a87cae274dc
Jesús <jesusfj710@proton.me>
2025-09-19 17:58:16 +0200

    Handle DATE-only DateTime inputs

--- src/vevent.cpp
+++ src/vevent.cpp
@@ -2,7 +2,6 @@
 # Copyright (c) 2020 Source Simian  :  https://github.com/sourcesimian/uICAL #
 ############################################################################*/
 #include "uICAL/cppstl.h"
-#include <cctype>
 #include "uICAL/types.h"
 #include "uICAL/util.h"
 #include "uICAL/datetime.h"
@@ -19,59 +18,8 @@ namespace uICAL {
         VLine_ptr rRule = obj->getPropertyByName("RRULE");
         VLine_ptr summary = obj->getPropertyByName("SUMMARY");
 
-        auto isDateOnly = [](const VLine_ptr& line) {
-            if (line == nullptr) {
-                return false;
-            }
-
-            string valueParam = line->getParam("VALUE");
-            if (!valueParam.empty()) {
-                string upper = valueParam;
-                std::transform(upper.begin(), upper.end(), upper.begin(), [](unsigned char ch) {
-                    return (char)std::toupper(ch);
-                });
-                if (upper == "DATE") {
-                    return true;
-                }
-            }
-
-            const string& value = line->value;
-            return value.find("T") == string::npos && value.length() == 8;
-        };
-
-        auto buildDateTimeValue = [&](const VLine_ptr& line, const string& fallbackTz) {
-            if (line == nullptr) {
-                return string::none();
-            }
-
-            string value = line->value;
-            if (isDateOnly(line) && value.length() == 8) {
-                value += "T000000";
-            }
-
-            string tz = line->getParam("TZID");
-            if (tz.empty()) {
-                tz = fallbackTz;
-            }
-
-            if (!tz.empty()) {
-                value += tz;
-            }
-
-            return value;
-        };
-
-        string dtStartValue = buildDateTimeValue(dtStart, string());
-        string dtStartTz = dtStart != nullptr ? dtStart->getParam("TZID") : string();
-        string dtEndValue = buildDateTimeValue(dtEnd, dtStartTz);
-
-        this->start = DateTime(dtStartValue, tzmap);
-        if (!dtEndValue.empty()) {
-            this->end = DateTime(dtEndValue, tzmap);
-        }
-        else {
-            this->end = this->start;
-        }
+        this->start = DateTime(dtStart->value + dtStart->getParam("TZID"), tzmap);
+        this->end = DateTime(dtEnd->value + dtStart->getParam("TZID"), tzmap);
 
         this->summary = summary->value;
 
